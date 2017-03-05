96ede0afdddb4186a2fd8195ba66f07edb811879
Minaz Abdulla <minaz.abdulla@gmail.com>
2017-03-04 23:20:11 -0500

    Add ability to choose a backfill buffer window

--- PHP/StreamingConnection.php
+++ PHP/StreamingConnection.php
@@ -5,6 +5,11 @@
     //The tracker unique hash is found in the tracker's url. ie http://keyhole.co/realtime/ABCDEF/ 
     $hash = 'abcdef'; 
 
+    // (Optional) The number of minutes to backfill/hold tweets. This is useful if you accidentally disconnect and want to
+    // backfill missing tweets. Note: tweets already received by the client will not be sent again upon re-connection. 
+    $buffer_minutes = 10;
+
+
     $this->curl = curl_init();
     $curl_params = array(
         CURLOPT_URL => 'https://stream.keyhole.co',
@@ -12,6 +17,7 @@
         CURLOPT_POSTFIELDS => http_build_query(array(
             'client_id' => $client_id,
             'hash' => $hash,
+            'buffer_minutes' => $buffer_minutes,
         )),
     );
 
--- Python/StreamingConnection.py
+++ Python/StreamingConnection.py
@@ -6,9 +6,12 @@ def streaming():
     
     data = {
         # Your api token is found at: http://keyhole.co/my_token
-        "client_id":"your_api_token", 
+        "client_id":"your_api_token",
         # The tracker unique hash is found in the tracker's url. ie http://keyhole.co/realtime/ABCDEF/ 
-        "hash":"abcdef"
+        "hash":"abcdef",
+	# (Optional) The number of minutes to backfill/hold tweets. This is useful if you accidentally disconnect and want to
+	# backfill missing tweets. Note: tweets already received by the client will not be sent again upon re-connection. 
+	"buffer_minutes":"10"
     }
 
     req = requests.Request('POST', 'https://stream.keyhole.co',data=data).prepare()
